7e9d999e45e349d590db63ea64a98dff033c2d96
Dylan Nguyen <dylannguyen1000@gmail.com>
2020-04-04 12:26:47 -0400

    Basic Login

---
+++ passport-config.js
@@ -0,0 +1,28 @@
+const localStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
+
+function initialize(passport, getUserByName, getUserById) {
+    const authenticateUser = async (name, password, done) => {
+        const user = getUserByName(name)
+        if (user == null) {
+            return done(null, false, { message: 'No user with that name'})
+        }
+
+    try {
+            if (await bcrypt.compare(password, user.password)) {
+                return done(null, user)
+            } else {
+                return (null, false, { message: 'Password incorrect'})
+            }
+        } catch (e) {
+            return done(e)
+        }
+    }  
+    passport.use(new localStrategy({ usernameField: 'name'}, authenticateUser))
+    passport.serializeUser((user, done) => done(null, user.id))
+    passport.deserializeUser((id, done) => {
+        return done(null, getUserById(id))
+    })
+}
+
+module.exports = initialize
